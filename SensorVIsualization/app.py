491acc00036ff4891008d2bb567697ade31a5401
H-Scorpion <haunghsien7@gmail.com>
2020-10-10 23:55:17 +0800

    fix green light problem

--- SensorVIsualization/app.py
+++ SensorVIsualization/app.py
@@ -1,7 +1,7 @@
-#debug
-import RPi.GPIO as GPIO
-from mfrc522 import SimpleMFRC522
-
+#-----comment-while-debug----------
+# import RPi.GPIO as GPIO
+# from mfrc522 import SimpleMFRC522
+#----------------------------------
 from flask import Flask,render_template,url_for,request,redirect, make_response
 import random
 import json
@@ -13,90 +13,93 @@ app = Flask(__name__)
 Blue = 29
 Pink = 31
 
-debug
-GPIO.setmode(GPIO.BOARD)
-# set up pin 11 as an output
-GPIO.setup(Blue, GPIO.OUT)
-GPIO.setup(Pink, GPIO.OUT)
+#-----comment-while-debug----------
+# GPIO.setmode(GPIO.BOARD)
+# # set up pin 11 as an output
+# GPIO.setup(Blue, GPIO.OUT)
+# GPIO.setup(Pink, GPIO.OUT)
+#----------------------------------
 
-# @app.route('/', methods=["GET", "POST"])
-# def main():
-#     print('state3')
-#     return render_template('index.html')
+@app.route('/', methods=["GET", "POST"])
+def main():
+    print('state3')
+    return render_template('index.html')
+# rfid num: obj_id
+rfid_dic={
+        385426309652 : 3,
+        896299244541 : 4
+    }
 
-
-# @app.route('/data', methods=["GET", "POST"])
-# def data():
-#     # Data Format [ Time, Velocity, SOC]
-#     Velocity = random()*150  
-#     SOC = random()*100
-#     # rfid num: id
-#     rfid_dic={       
-#         385426309652 : 0
-#     }
-    
-#     tmp1=input("id")
-#     tmp2=input("status")
-#     tmp3=input("tmp3")
-
-#     # id mapping 
-#     # 0:wallet
-#     # 1:phone
-    
-
-#     #data = [time() * 1000, Velocity, SOC]
-#     data = [tmp1,tmp2,tmp3]
-#     #define variable rfid_recv
-
-#     # rfid_recv = [ id , status , 0]
-
-#     #response = make_response(json.dumps(rfid_recv))
-#     response = make_response(json.dumps(data))
-#     response.content_type = 'application/json'
-
-#     return response
-
-
-# if __name__ == "__main__":
-#     print(__name__)
-#     app.run(debug=True, port=8888)
-
+#---------------for debug--------------------
 @app.route('/data', methods=["GET", "POST"])
 def data():
-    print('state1')
-    
-    reader = SimpleMFRC522()
-    GPIO.output(Blue,False)
-    GPIO.output(Pink,True)
-    id, text = reader.read()
-    
-    # rfid num: id
-    rfid_dic={
-            385426309652 : 3,
-            896299244541 : 4
-            }
-    
     # Data Format [ Time, Velocity, SOC]
+    Velocity = random()*150  
+    SOC = random()*100
+
+    rfid_num=input("rfid_num:")
+    status=input("status:")
+    tmp3=input("tmp3")
+
+    # id mapping 
+    # 0:wallet
+    # 1:phone
     
-    if text != ' ':
-        GPIO.output(Pink,False)
-        GPIO.output(Blue,True)
-        print(id)
-        print(text)
-        data = [rfid_dic[id], text, time.time()]
-        time.sleep(1)
-        
-    print('state2')
+
+    #data = [time() * 1000, Velocity, SOC]
+    data = [rfid_num,status,tmp3]
+    #define variable rfid_recv
+
+    # rfid_recv = [ id , status , 0]
+
+    #response = make_response(json.dumps(rfid_recv))
     response = make_response(json.dumps(data))
-        
     response.content_type = 'application/json'
-        
+
     return response
 
 
 if __name__ == "__main__":
-    try:
-        app.run(debug=True)
-    finally:
-        GPIO.cleanup()
+    print(__name__)
+    app.run(debug=True, port=8888)
+#------------------------------------------
+
+
+
+#---------------for rpi (comment while debug)--------------------
+# @app.route('/data', methods=["GET", "POST"])
+# def data():
+#     print('state1')
+    
+#     reader = SimpleMFRC522()
+#     GPIO.output(Blue,False)
+#     GPIO.output(Pink,True)
+#     rfid_num, text = reader.read()
+    
+
+    
+#     # Data Format [ Time, Velocity, SOC]
+    
+#     if text != ' ':
+#         GPIO.output(Pink,False)
+#         GPIO.output(Blue,True)
+#         print(rfid_num)
+#         print(text)
+#         data = [rfid_dic[rfid_num], text, time.time()]
+#         time.sleep(1)
+        
+#     print('state2')
+#     response = make_response(json.dumps(data))
+        
+#     response.content_type = 'application/json'
+        
+#     return response
+
+
+# if __name__ == "__main__":
+#     try:
+#         app.run(debug=True)
+#     finally:
+#         GPIO.cleanup()
+#--------------------------------
     
